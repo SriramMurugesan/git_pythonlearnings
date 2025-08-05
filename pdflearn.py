b7c6cd6aa472634cdad641d58d7ce82962fddf1d
SriramMurugesan <sri2014ram@gmail.com>
2025-08-05 12:32:42 +0530

    pdf extraction

--- pdflearn.py
+++ pdflearn.py
@@ -1,11 +1,19 @@
 import os
 import pymupdf
 
-path=r"/home/sriram/doc"
-file=os.listdir(path)
+path = r"/home/sriram/doc"
+file = os.listdir(path)
+
+sql_keywords = ('SELECT', 'INSERT', 'UPDATE', 'DELETE', 'CREATE', 'ALTER', 'DROP', 'TRUNCATE')
+
 for i in file:
-    data1=os.path.join(path,i)
-    pdf=pymupdf.open(data1)
+    data1 = os.path.join(path, i)
+    pdf = pymupdf.open(data1)
     for j in pdf:
-        data=j.get_text().strip().splitlines()
-        print(data)
+        data = j.get_text().strip().splitlines()
+        # print(data)
+        for k in data:
+            line = k.strip()
+            if any(line.upper().startswith(keyword) for keyword in sql_keywords):
+                sql_query = line
+                print(sql_query)
