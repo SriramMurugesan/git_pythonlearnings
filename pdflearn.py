3f482bf6c8dcadd83f70efb192bf78d7e3fda9a9
SriramMurugesan <sri2014ram@gmail.com>
2025-08-05 12:18:44 +0530

    pdf extract

--- pdflearn.py
+++ pdflearn.py
@@ -1,9 +1,11 @@
 import os
 import pymupdf
 
-sqldata=r"D:\doc"
-file=os.listdir(sqldata)
+path=r"/home/sriram/doc"
+file=os.listdir(path)
 for i in file:
-    data_ex=os.path.join(sqldata,i)
-    full_data=pymupdf.open(data_ex)
-    print(full_data)
+    data1=os.path.join(path,i)
+    pdf=pymupdf.open(data1)
+    for j in pdf:
+        data=j.get_text().strip().splitlines()
+        print(data)
